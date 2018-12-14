fef03739fe9322504a7a1de6a94f4b49f3452078
xia-fei <xiafei.dev@gmail.com>
2018-12-14 19:55:38 +0800

    aa

--- src/main/java/org/xiafei/git/webhooks/MainController.java
+++ src/main/java/org/xiafei/git/webhooks/MainController.java
@@ -3,10 +3,14 @@ package org.xiafei.git.webhooks;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class MainController {
     @RequestMapping
-    public String main(){
+    public String main() throws IOException {
+        System.out.println("请求来了");
+       Process process= Runtime.getRuntime().exec("/root/xiafei/webhtml.sh 1");
         return "ok";
     }
 }
