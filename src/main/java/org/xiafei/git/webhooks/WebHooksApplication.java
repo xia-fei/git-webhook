028947e144505dbc6221bc23b4886c7b68ef6e11
xia-fei <xiafei.dev@gmail.com>
2018-12-14 19:50:23 +0800

    first

---
+++ src/main/java/org/xiafei/git/webhooks/WebHooksApplication.java
@@ -0,0 +1,14 @@
+package org.xiafei.git.webhooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebHooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebHooksApplication.class, args);
+    }
+
+}
+
